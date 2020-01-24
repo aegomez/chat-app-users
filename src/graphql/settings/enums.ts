bd8a43276e7a46aad81d8476f674756da65ceae0
Adrian Gomez <aegomez@tekmexico.com>
2020-01-24 12:15:00 -0800

    --amend

--- src/graphql/settings/enums.ts
+++ src/graphql/settings/enums.ts
@@ -3,6 +3,7 @@ import { GraphQLEnumType } from 'graphql';
 export const userLanguageEnum = new GraphQLEnumType({
   name: 'UserLanguageEnum',
   values: {
+    auto: { value: 'auto' },
     en: { value: 'en' },
     es: { value: 'es' },
     pt: { value: 'pt' }
